169ac007816c15fa11d398a700d19134fde9c06a
Akihisa Yagi <capra314cabra@gmail.com>
2021-09-06 20:45:41 +0900

    Shape IrohaOrderEditor to a dialog form

--- lib/order-editor/main.dart
+++ lib/order-editor/main.dart
@@ -4,76 +4,88 @@ import "package:iroha/foods-table/main.dart";
 import "package:iroha/main.dart";
 
 class IrohaOrderEditor extends StatefulWidget {
-	final Map<String, int> _menuItemCounter = <String, int>{ };
+	final void Function(Map<String, int> order) onOrderUpdated;
 
-	IrohaOrderEditor({Key? key}) : super(key: key);
+	IrohaOrderEditor({required this.onOrderUpdated, Key? key}) : super(key: key);
 
 	@override
     _IrohaOrderEditorState createState() => _IrohaOrderEditorState();
 }
 
 class _IrohaOrderEditorState extends State<IrohaOrderEditor> {
+	final Map<String, int> _menuItemCounter = <String, int>{ };
+
     @override
     Widget build(BuildContext context) {
-        return ListView(
+        return _buildContent(context);
+    }
+
+	Widget _buildContent(BuildContext context) {
+		return Column(
+			mainAxisAlignment: MainAxisAlignment.start,
+			mainAxisSize: MainAxisSize.min,
 			children: <Widget>[
-				Center(
-					child: Text(
-						"注文をとる",
-						style: TextStyle(
-							fontSize:30
-						)
+				Text(
+					"1番テーブル",
+					style: TextStyle(
+						fontSize: 25
 					)
 				),
 				Container(
-					margin: EdgeInsets.all(20),
-					decoration: BoxDecoration(
-						border: Border.all(color: Colors.blue),
-						borderRadius: BorderRadius.circular(20)
-					),
-					child: Container(
-						margin: EdgeInsets.all(20),
-						child: Consumer(
-							builder: (context, watch, child) {
-								var menu = watch(menuDataProvider);
-
-								if (widget._menuItemCounter.isEmpty) {
-									for (var counter = 0; counter < menu.length; counter++) {
-										widget._menuItemCounter[menu.elementAt(counter)] = 0;
-									}
-								}
-
-								return IrohaFoodsTable(
-									data: menu,
-									foodNameFromItem: (String item) {
-										return item;
-									},
-									counterFromItem: (String item) {
-										return DropdownButton<int>(
-											value: widget._menuItemCounter[item],
-											items: [
-												for (int i = 0; i <= 5; i++)
-													DropdownMenuItem(
-														child: Text(
-															i.toString(),
-															style: TextStyle(fontSize: 25)
-														),
-														value: i,
-													)
-											],
-											onChanged: (value) {
-												setState(() {
-													widget._menuItemCounter[item] = value ?? 0;
-												});
-											}
-										);
-									}
-								);
-							}
-						)
-					)
+					margin: EdgeInsets.all(10),
+					height: 2,
+					color: Colors.blue
+				),
+				Consumer(
+					builder: _buildFoodsTable
+				),
+				Container(
+					margin: EdgeInsets.all(10),
+					height: 2,
+					color: Colors.blue
 				)
 			]
 		);
-    }
+	}
+
+	Widget _buildFoodsTable(BuildContext context, ScopedReader watch, Widget? child) {
+		var menu = watch(menuDataProvider);
+
+		if (_menuItemCounter.isEmpty) {
+			for (var counter = 0; counter < menu.length; counter++) {
+				_menuItemCounter[menu.elementAt(counter)] = 0;
+			}
+
+			widget.onOrderUpdated(_menuItemCounter);
+		}
+
+		return IrohaFoodsTable(
+			data: menu,
+			foodNameFromItem: (String item) {
+				return item;
+			},
+			counterFromItem: (String item) {
+				return DropdownButton<int>(
+					value: _menuItemCounter[item],
+					items: [
+						for (int i = 0; i <= 5; i++)
+							DropdownMenuItem(
+								child: Text(
+									i.toString(),
+									style: TextStyle(fontSize: 25)
+								),
+								value: i,
+							)
+					],
+					onChanged: (value) {
+						setState(() {
+							_menuItemCounter[item] = value ?? 0;
+
+							widget.onOrderUpdated(_menuItemCounter);
+						});
+					}
+				);
+			}
+		);
+	}
 }
