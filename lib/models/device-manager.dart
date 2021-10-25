b15fd066b89ee72c6171ba0ca0d52ba22aa812eb
Akihisa Yagi <capra314cabra@gmail.com>
2021-10-25 19:49:00 +0900

    Use device manager to detect the OS

--- lib/models/device-manager.dart
+++ lib/models/device-manager.dart
@@ -1,5 +1,13 @@
+import "dart:io";
+
+import "package:flutter/foundation.dart";
+
 class DeviceManager {
 	static bool isAdmin() {
 		return const bool.fromEnvironment("ADMIN");
+	}
+
+	static bool isIOS() {
+		return !kIsWeb && Platform.isIOS;
 	}
 }
